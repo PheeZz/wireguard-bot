5ae9050bb39cfc9322fe54d29f399d662130a59a
Daniil <53043573+PheeZz@users.noreply.github.com>
2023-11-04 12:11:58 +0200

    Update SemiAutoInstall.sh: add `enp0s5` interface

--- SemiAutoInstall.sh
+++ SemiAutoInstall.sh
@@ -191,6 +191,8 @@ if ip link show eth0 &> /dev/null; then
     interface=eth0
 elif ip link show ens3 &> /dev/null; then
     interface=ens3
+elif ip link show enp0s5 &> /dev/null; then
+    interface=enp0s5
 else
     echo "Error: could not determine network interface" >&2
     exit 1
